d0e6bcbb19cfb223c5efd0eb508f4980c8aa1495
Mehrnaz Khorrami <mehrnazkhorrami769@gmail.com>
2025-10-26 00:59:48 +0330

    feat: try express

--- server.js
+++ server.js
@@ -1,25 +1,34 @@
-const http = require("http");
+import express from "express";
 
-const server = http.createServer((req, res) => {
-  if (req.url === "/") {
-    res.writeHead(200, { "Content-Type": "text/plain" });
-    res.end("Hello Node");
-  } else if (req.url === "/time") {
-    res.writeHead(200, { "Content-Type": "text/plain" });
-    res.end(`Current time: ${new Date().toLocaleTimeString()}`);
-  } else if (req.url === "/about") {
-    res.writeHead(200, { "Content-Type": "text/plain" });
-    res.end("Mehrnaz Khorrami");
-  } else if (req.url === "/random") {
-    res.writeHead(200, { "Content-Type": "text/plain" });
-    const myRandom = Math.floor(Math.random() * (100 - 1) + 1);
-    res.end(myRandom.toString());
-  } else {
-    res.writeHead(404, { "Content-Type": "text/plain" });
-    res.end("Not Found");
-  }
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("Hello from express");
 });
 
-server.listen(3000, () => {
+app.get("/time", (req, res) => {
+  res.send(`Current time ${new Date().toLocaleString()}`);
+});
+
+app.get("/about", (req, res) => {
+  res.send(`My name is Mehrnaz, Now it's ${new Date().toLocaleString()}`);
+});
+
+app.get("/random", (req, res) => {
+  const randomNumber = Math.floor(Math.random() * 100) + 1;
+  res.send(randomNumber.toString());
+});
+
+app.get("/greet/:name", (req, res) => {
+  const { name } = req.params;
+  res.send(`Hello, ${name}`);
+});
+
+app.get("/sum", (req, res) => {
+  const {a, b} = req.query;
+  const sum = Number(a) + Number(b)
+  res.send(sum.toString())
+})
+app.listen(3000, () => {
   console.log("Server running at http://localhost:3000");
 });
